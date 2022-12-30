fa1018f86f86f11ad066bbacb5eaabbd54148a5c
Samar <smr.aloufii@gmail.com>
2022-12-30 10:59:50 +0300

    naafh

---
+++ calcapp.js
@@ -0,0 +1,32 @@
+
+
+const keys = document.querySelector('.calc-key');
+
+const display = document.getElementById('display');
+
+
+keys.addEventListener('click' , (event) => {
+
+ let target = event.target;
+
+ if(!target.matches('button')) {return false;}
+
+if(target.classList.contains('equal')){
+
+try {
+  let result = eval(display.value);
+  display.value = result;
+
+}catch(e){
+  if (e instanceof SyntaxError){
+    alert('ERROR');
+    display.value=''
+  }
+}
+}else if (target.classList.contains('all-clear')) {
+    display.value = '' ;
+  }else {
+
+ display.value += target.value;
+}
+ });
